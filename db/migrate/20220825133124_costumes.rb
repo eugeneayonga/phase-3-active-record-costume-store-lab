401a9e379092b216fe30b498273b81bcc22b4ab1
Eugene <eugeneayonga@gmail.com>
2022-08-25 17:02:49 +0300

    commit tree 85be11419a2e5718de846ed3a87b01357f09155d

---
+++ db/migrate/20220825133124_costumes.rb
@@ -0,0 +1,11 @@
+class Costumes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.string :image_url
+      t.integer :size
+      t.timestamps
+    end
+  end
+end
